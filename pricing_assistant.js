3366de024c7830cd354f23f63123747e13f7ffaf
Sofia Serrano <syserrano@usf.edu>
2026-01-30 22:12:38 -0500

    first commit

---
+++ pricing_assistant.js
@@ -0,0 +1,21 @@
+// product name
+const productName = "Star Paper"
+console.log(`The product is called ${productName}.`)
+
+let costPerUnit = 7;
+
+const BASE_PRICE = 5;
+
+let discountRate = 0.1;
+
+const SALES_TAX_RATE = 0.07;
+
+const FIXED_MONTHLY_COSTS = 100;
+
+// discounted price before tax
+let discountedPrice = BASE_PRICE * (1 - discountRate);
+console.log(`Discounted price before tax is $${discountedPrice.toFixed(2)}.`);
+
+let finalPriceWithTax = discountedPrice * (1 + SALES_TAX_RATE);
+
+let 
