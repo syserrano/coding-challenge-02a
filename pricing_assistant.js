8ea80a0ec796511991fb20c65fd41abfd07f6f09
Sofia Serrano <syserrano@usf.edu>
2026-01-30 22:30:39 -0500

    Added final price with tax and profit per unit

--- pricing_assistant.js
+++ pricing_assistant.js
@@ -2,9 +2,9 @@
 const productName = "Star Paper"
 console.log(`The product is called ${productName}.`)
 
-let costPerUnit = 7;
+let costPerUnit = 2.5;
 
-const BASE_PRICE = 5;
+const BASE_PRICE = 7;
 
 let discountRate = 0.1;
 
@@ -16,6 +16,11 @@ const FIXED_MONTHLY_COSTS = 100;
 let discountedPrice = BASE_PRICE * (1 - discountRate);
 console.log(`Discounted price before tax is $${discountedPrice.toFixed(2)}.`);
 
+// final price with tax
 let finalPriceWithTax = discountedPrice * (1 + SALES_TAX_RATE);
+console.log(`Final price with tax is $${finalPriceWithTax.toFixed(2)}.`);
 
-let 
+// profit per unit
+let profitPerUnit = finalPriceWithTax - costPerUnit;
+console.log(`Profit per unit is $${profitPerUnit.toFixed(2)}.`);
+
